89e7e48bbd981a1dbf72ede248bf793853c3ee2f
Mrwicked <victoradeoba83@gmail.com>
2024-04-15 09:03:29 +0100

    donepro

--- script.js
+++ script.js
@@ -29,28 +29,35 @@ const fetchWeather = async () => {
             document.getElementById('cityName').innerHTML = data.name
             document.querySelector('.humidity').innerHTML = data.main.humidity + '%'
             document.querySelector('.wind').innerHTML = data.wind.speed + 'km/h'
+            document.getElementById('cityAbb').innerHTML = data.sys.country
+            console.log(data);
+            const weatherIconCode = data.weather[0].icon;
+            weatherImg.src = `http://openweathermap.org/img/wn/${weatherIconCode}.png`;
+    
 
-            if (data.weather[0].main == 'Clouds') {
-                weatherImg.src = 'images/clouds.png'
-            }else if (data.weather[0].main == 'Rain') {
-                weatherImg.src = 'images/rain.png'
+            // weatherImg.src = data.weather[0].icon
+
+            // if (data.weather[0].main == 'Clouds') {
+            //     weatherImg.src = 'images/clouds.png'
+            // }else if (data.weather[0].main == 'Rain') {
+            //     weatherImg.src = 'images/rain.png'
                 
-            }else if (data.weather[0].main == 'Drizzle') {
-                weatherImg.src = 'images/drizzle.png'
+            // }else if (data.weather[0].main == 'Drizzle') {
+            //     weatherImg.src = 'images/drizzle.png'
 
-            }else if (data.weather[0].main == 'Mist') {
-                weatherImg.src = 'images/mist.png'
-                body.style.backgroundImage = 'url("images/pexels-sam-forson-154246.jpg")'
+            // }else if (data.weather[0].main == 'Mist') {
+            //     weatherImg.src = 'images/mist.png'
+            //     body.style.backgroundImage = 'url("images/pexels-sam-forson-154246.jpg")'
 
-            }else if (data.weather[0].main == 'Snow') {
-                weatherImg.src = 'images/snow.png'
+            // }else if (data.weather[0].main == 'Snow') {
+            //     weatherImg.src = 'images/snow.png'
 
-            }else if (data.weather[0].main == 'Clear') {
-                weatherImg.src = 'images/clear.png'
+            // }else if (data.weather[0].main == 'Clear') {
+            //     weatherImg.src = 'images/clear.png'
 
-            }else{
-                console.error('no data found');
-            }
+            // }else{
+            //     console.error('no data found');
+            // }
             document.getElementById('weatherImg').style.display = 'block'
             document.getElementById('searchCity').value = ''
             let error =document.getElementById('searchCity')
